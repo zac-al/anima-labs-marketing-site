7c8e46c37acc5505638526044b1edf6bb3461bad
zac-al <zac@animalabs.io>
2023-06-16 15:34:50 +0100

    setup: installed router

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import { Routes, Route } from "react-router-dom"
+import { Home } from './container'
+
+function App() {
+  return (
+    <div className="App">
+    <Routes>
+      <Route path="/" element={ <Home/> } />
+    </Routes>
+    </div>
+  );
+}
+
+export default App;
